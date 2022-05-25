9b69e3b15c5d2bc7f7b6bc61f270ec6d7ba8cd5c
ThienNguyen27 <nguyenleminhthien27092005@gmail.com>
2022-05-25 22:49:24 +0700

    daddy is gay

---
+++ test.js
@@ -0,0 +1,5 @@
+// console.log("Hello"); printing hello
+// const prompt =require('prompt-sync')();
+// var firstname=prompt("Enter your first name: ");
+// var lastname=prompt("Enter your last name: ");
+// console.log("Hello, ",firstname,lastname); user's input
